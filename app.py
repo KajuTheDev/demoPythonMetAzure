a48a640f0bc2bee9bf14e6904747009ca56874ce
KajuTheDev <113180572+KajuTheDev@users.noreply.github.com>
2022-11-08 12:36:23 +0100

    tweede commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World! Dit is de tweede commit</p>"
 
 @app.route("/andereURL")
 def hello_world2():
